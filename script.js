9b016ed0ad6bd95d84d9624bb0c50dfdbe0bccf8
Dozext <stefanoborja2020@gmail.com>
2026-02-21 22:18:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -34,17 +34,17 @@ setInterval(createPetal, 300);
 
 const texto = `Mi Danielita,
 
-Hoy celebramos tus 19 añitos, me encuentro buscando palabras que sean lo suficientemente grandes para describir lo que causas en mí, pero la verdad es que "te quiero" se queda demasiado corto. Me tienes completa y perdidamente embobado. A veces siento que mi corazón ya no me pertenece, porque hace mucho tiempo salió de mi pecho para quedarse en tus manos, y te juro que no existe lugar más seguro en el universo que ahí, contigo.
+Hoy celebras tus 19 añitos, la verdad todo el dia me pase buscando las palabras que sean lo suficientemente grandes para describir lo que causas en mí, pero la verdad es que "te quiero" se queda demasiado corto. Me tienes completa y perdidamente embobado. A veces siento que mi corazón ya no me pertenece, porque desde el primer momento que te vi salió de mi pecho para quedarse en tus manos, y te juro que no existe lugar más seguro en el universo que ahí, contigo.
 
-¿Sabes? Cuando estoy a tu lado, la realidad se desvanece. Mi mente y mi corazón entran en una película de fantasía. Tú eres mi magia en un mundo muggle. Eres mi Patronus: ese recuerdo feliz, potente y luminoso que es capaz de alejar cualquier oscuridad, cualquier tristeza y hacerme sentir invencible.
+Cuando estoy a tu lado, mi realidad se desvanece. Me transformas de una en una pelicula de fantasia. Tú eres mi magia dentro de un mundo muggle. Eres mi Patronus, Tu eres ese recuerdo feliz, potente y luminoso que es capaz de alejar cualquier oscuridad, cualquier tristeza y hacerme sentir invencible.
 
 Quiero que sepas que cada canción que te dedico, como esta que suena ahora, no es solo música. Cada letra viaja desde la capa más visible de mi piel hasta lo más profundo e inexplorado de mi alma, llevando mensajes que a veces mi voz no alcanza a decir.
 
-Y si alguna vez, por mi torpeza o mi intensidad, te he hecho sentir mal o avergonzada con mi comportamiento, te pido perdón desde lo más hondo de mi ser. Soy humano y me equivoco, pero quiero que estes segura de algo, mi misión en esta vida es cuidarte. Quiero ser tu escudo y protegerte de todo mal hasta el fin de mis días.
+Y si alguna vez, por mi torpeza o mi intensidad, te he hecho sentir mal o avergonzada con mi comportamiento, te pido perdón desde lo más profundo de mi ser. Soy humano,hombre (aunque me den asco jajaja) y me equivoco, pero quiero que estes segura de algo, mi misión en esta vida es cuidarte. Quiero ser tu escudo y protegerte de todo mal hasta el fin de mis días.
 
 Deseo que este cumpleaños sea tan hermoso como tu sonrisa. Te mereces todo lo bueno de este mundo y más.
 
-Con Amor, Stefano`;
+Con Amor, Stefano;
 
 let index = 0;
 function escribirCarta() {
@@ -55,4 +55,5 @@ function escribirCarta() {
         index++;
         setTimeout(escribirCarta, 50); 
     }
+
 }
